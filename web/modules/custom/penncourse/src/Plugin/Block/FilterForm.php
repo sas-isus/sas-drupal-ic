f5ac43762110c3d6ff147083b71373e6318d86cc
Pekens Antoine <pekens@upenn.edu>
2023-11-06 22:27:07 -0500

    add: updated penncourse module, private/simplesamlphp directory, settings files

---
+++ web/modules/custom/penncourse/src/Plugin/Block/FilterForm.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\penncourse\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\penncourse\Service\PenncourseService;
+use Drupal\Core\Render\Renderer;
+
+/**
+ * Provides a 'FilterForm' block.
+ *
+ * @Block(
+ *  id = "penncourse_filter_form",
+ *  admin_label = @Translation("Course Filter"),
+ * )
+ */
+class FilterForm extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\penncourse\Service\PenncourseService definition.
+   *
+   * @var \Drupal\penncourse\Service\PenncourseService
+   */
+  protected $penncourseService;
+  protected $renderer;
+
+  /**
+   * Constructs a new FilterForm object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    PenncourseService $penncourse_service,
+    Renderer $renderer
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->penncourseService = $penncourse_service;
+    $this->renderer = $renderer;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('penncourse.service'),
+      $container->get('renderer')
+    );
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // $form = \Drupal::formBuilder()->getForm('Drupal\penncourse\Form\PenncourseFilterForm');
+    // return $form;
+    $build = [];
+    $f = \Drupal::formBuilder()->getForm('Drupal\penncourse\Form\PenncourseFilterForm');
+    $build['#markup'] = $this->renderer->render($f);    
+    $build['#attached']['library'][] = 'penncourse/penncourse-form';
+
+    return $build;
+  }
+
+}
---
+++ web/private/simplesamlphp/config/authsources.php
@@ -0,0 +1,48 @@
+<?php
+
+// getting environment info
+
+if (isset($_ENV['PANTHEON_SITE_NAME'])) {
+	$SiteName = $_ENV['PANTHEON_SITE_NAME'];
+}
+else {
+	$SiteName = 'local';
+}
+
+if (file_exists($_SERVER['DOCUMENT_ROOT']. '/sites/default/settings.sas-functions.php')) {
+        require_once($_SERVER['DOCUMENT_ROOT']. '/sites/default/settings.sas-functions.php');
+}
+
+$EntityID = getEntityID();
+
+$config = array(
+
+	// This is a authentication source which handles admin authentication.
+	'admin' => array(
+		// The default is to use core:AdminPassword, but it can be replaced with
+		// any authentication source.
+
+		'core:AdminPassword',
+	),
+	// An authentication source which can authenticate against both SAML 2.0
+	// and Shibboleth 1.3 IdPs.
+	'default-sp' => array(
+    'saml:SP',
+		'privatekey' => $SiteName . '.pem',
+		'certificate' => $SiteName . '.crt',
+		'entityID' => $EntityID,
+		'idp' => 'https://idp.pennkey.upenn.edu/idp/shibboleth',
+		'discoURL' => NULL,
+		'authproc' => array(
+			50 => array( // map attributes to names rather than numeric ids
+				'class' => 'core:AttributeMap', 'oid2name'
+			),
+			60 => array( // replace colons in group names
+				'class'   => 'core:AttributeAlter',
+				'subject' => 'eduPersonEntitlement',
+				'pattern' => '/:/',
+				'replacement' => '.'
+			),
+		),
+	),
+);
